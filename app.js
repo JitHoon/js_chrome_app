275014883a11c7994afec6626cc0dc8725f0f0e1
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-02-27 15:42:24 +0900

    Hide elements using css display:noe, element.classList.add, remove

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-loginForm = document.querySelector("#login-form")
-loginInput = document.querySelector("#login-form input")
+loginForm = document.querySelector("#login-form");
+loginInput = document.querySelector("#login-form input");
+greeting = document.querySelector("#login-form");
 
 function func(event) {
-    // 브라우저의 기본 HTML 동작을 하지 않도록 막는 method
-    event.preventDefault()
-    // submit한 input의 value를 console에 계속 띄울 수 있게됨.
-    console.log(loginInput.value)
-}
+    event.preventDefault();
+    userName = loginInput.value;
+    // css display: none 과 
+    // element.classList.add, remove를 활용한 Interation
+    loginForm.classList.add("hidden");
+    greeting.classList.remove("hidden");
+};
 
 // form의 submit의 기본 동작은 브라우저를 새로고침한다.
-loginForm.addEventListener("submit", func)
+loginForm.addEventListener("submit", func);
