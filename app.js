45888099d20de3214d9c8ec2c0f87912bd7d3ae8
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-02-27 16:18:12 +0900

    Streamline LOGIN

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-loginForm = document.querySelector("#login-form");
-loginInput = document.querySelector("#login-form input");
-greeting = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#login-form");
+
+const USER_NAME_KEY = "userName"
+const CSS_HIDDEN = "hidden"
 
 function func(event) {
     event.preventDefault();
     const userName = loginInput.value;
     // 브라우저에서 제공하는 localStorage를 활용하여 userName 저장
-    localStorage.setItem("userName",userName)
-    // css display: none 과 
-    // element.classList.add, remove를 활용한 Interation
-    loginForm.classList.add("hidden");
-    greeting.classList.remove("hidden");
-    // `${변수}`
-    greeting.innerText = `✨ Hello ${userName} 📣🐰`;
+    localStorage.setItem(USER_NAME_KEY,userName)
+    loginForm.classList.add(CSS_HIDDEN);
+    showGreeting(userName)
 };
 
-// form의 submit의 기본 동작은 브라우저를 새로고침한다.
-loginForm.addEventListener("submit", func);
+function showGreeting(name) {
+    greeting.classList.remove(CSS_HIDDEN);
+    // `${변수}`
+    greeting.innerText = `✨ Hello ${name} 📣🐰`;
+}
+
+// localStorage에 userName 여부 확인
+const savedUsername = localStorage.getItem(USER_NAME_KEY)
+
+if (savedUsername === null) {
+    loginForm.classList.remove(CSS_HIDDEN);
+    // form의 submit의 기본 동작은 브라우저를 새로고침한다.
+    loginForm.addEventListener("submit", func);
+} else {
+    showGreeting(savedUsername)
+};
