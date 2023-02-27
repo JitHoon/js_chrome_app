425338e51a479043e4125a494cffceaa5a814a3f
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-02-27 14:45:16 +0900

    get HTNL element value when click event (element.value)

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-// const, let
-const a = 1;
-let b = 1;
-b = 2
-console.log(a, b);
+loginInput = document.querySelector("#form input")
+loginBnt = document.querySelector("#form button")
 
-// null
-const c = null;
-console.log(c);
+function func() {
+    console.log(loginInput.value)
+}
 
-// undifined
-let d;
-console.log(d);
-
-// 설명이 필요하지 않은 데이터 리스트들은 array로,
-// 설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
-
-// array []
-const arr = [1, 2, true, 'hoonys', "hello"];
-console.log(arr);
-
-// object {}
-const player = {
-    name : 'hooonys',
-    point : 10,
-};
-
-player.point = 15;
-player.country = 'Korea';
-
-console.log(player);
-console.log(player.name);
-
-/* 
-    function name() {}, 
-    prompt(”질문”) = 파이썬 input()
-    typeof
-    parseInt()
-    isNaN()
-    if () {} else if () {} else {}
-    &&, ||, ===
-*/
+loginBnt.addEventListener("click", func)
