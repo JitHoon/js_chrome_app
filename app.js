94887fea89de35261115868a8b2d7a5a21c20185
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-02-27 15:55:45 +0900

    Saving userName using loclaStorage.setItem(key,value)

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ greeting = document.querySelector("#login-form");
 
 function func(event) {
     event.preventDefault();
-    userName = loginInput.value;
+    const userName = loginInput.value;
+    // 브라우저에서 제공하는 localStorage를 활용하여 userName 저장
+    localStorage.setItem("userName",userName)
     // css display: none 과 
     // element.classList.add, remove를 활용한 Interation
     loginForm.classList.add("hidden");
