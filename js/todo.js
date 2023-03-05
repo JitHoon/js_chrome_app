203f93022b181f73288b172fb8ed40dbda75e7cf
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-03-05 11:52:30 +0900

    Save array in local storage using l.setItem(, JSON.strungfy())

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("to-do-form");
 const toDoInput = document.querySelector("#to-do-form input");
 const toDoList = document.getElementById("to-do-list");
 
+const toDos = [];
+
+function toDoSave() {
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
 function toDoRemove(event) {
     // console.log(event); event -> target
     // console.dir(event.target); event.target -> parentElement
@@ -26,8 +32,10 @@ function toDoCreate(newTodo) {
 function toDoValue(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
+    toDos.push(newTodo)
     toDoInput.value = ""
     toDoCreate(newTodo);
+    toDoSave();
 }
 
 toDoForm.addEventListener("submit", toDoValue);
