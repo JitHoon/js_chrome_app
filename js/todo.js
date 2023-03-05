d01ce4e2b397d46ac413881aa264d828b8110abf
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-03-05 11:29:53 +0900

    remove element using event.target.parentElement.remove()

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,25 @@ const toDoForm = document.getElementById("to-do-form");
 const toDoInput = document.querySelector("#to-do-form input");
 const toDoList = document.getElementById("to-do-list");
 
+function toDoRemove(event) {
+    // console.log(event); event -> target
+    // console.dir(event.target); event.target -> parentElement
+    toDoTarget = event.target.parentElement;
+    toDoTarget.remove();
+}
+
 function toDoCreate(newTodo) {
     const toDoLi = document.createElement("li");
     const toDoSpan = document.createElement("span");
-    toDoLi.appendChild(toDoSpan);
     toDoSpan.innerText = newTodo;
+    const toDoBotton = document.createElement("botton");
+    toDoBotton.innerText = "𝗫";
+
+    toDoLi.appendChild(toDoSpan);
+    toDoLi.appendChild(toDoBotton);
     toDoList.appendChild(toDoLi);
+
+    toDoBotton.addEventListener("click", toDoRemove);
 }
 
 function toDoValue(event) {
